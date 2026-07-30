e7e10be4cc74e33e70c81b0e42e74af7b5616cd1
Baby akshatha <102918841+ashhureddy@users.noreply.github.com>
2026-07-30 16:34:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -49,10 +49,10 @@ TPL_DSS_4SECTOR = resolve_dss_template("standard")
 TPL_DSS_3SECTOR = resolve_dss_template("stand")
 
 TDIR_N2E = Path(__file__).parent / "templates" / "N2E"
-TPL_N2E_LTE = TDIR_N2E / "N2E_LTE_Integration_Procedure_with_LTE_Node_as_Primary_V4.txt"
-TPL_N2E_5G = TDIR_N2E / "N2E_5G_Integration_Procedure_with_5G_Node_as_Primary_V4.txt"
-TPL_N2E_MMBB = TDIR_N2E / "MMBB_N2E_Integration_Procedure_with_LTE_or_5G_Node_as_Primary_CMCLI_Updated_V6.txt"
-TPL_N2E_TRIMODE = TDIR_N2E / "N2E_TRIMODE_Integration_Procedure_with_LTE_or_5G_Node_as_Primary_CMCLI_Updated_V6.txt"
+TPL_N2E_LTE = TDIR_N2E / "N2E_LTE_Integration_Procedure_with_LTE_Node_as_Primary_V5.txt"
+TPL_N2E_5G = TDIR_N2E / "N2E_5G_Integration_Procedure_with_5G_Node_as_Primary_V6.txt"
+TPL_N2E_MMBB = TDIR_N2E / "MMBB_N2E_Integration_Procedure_with_LTE_or_5G_Node_as_Primary_CMCLI_Updated_V8"
+TPL_N2E_TRIMODE = TDIR_N2E / "N2E_TRIMODE_Integration_Procedure_with_LTE_or_5G_Node_as_Primary_CMCLI_Updated_V8.txt"
 
 TDIR_NSB = Path(__file__).parent / "templates" / "NSB"
 TDIR_STATIC = Path(__file__).parent / "templates" / "Static"
